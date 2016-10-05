0693a44b86421d051af8e6497a1998df6abe62c2
sergi <sergirenom@hotmail.com>
2016-10-05 12:45:28 +0200

    getting data from request

--- app/Controller/MathsController.php
+++ app/Controller/MathsController.php
@@ -5,12 +5,13 @@
 
           if($this->request->is('post'))
           {
-              $sum1 = $_POST['summand1'];
-              $sum2 = $_POST['summand2'];
+              $sum1 = $this->request->data['summand1'];
+              $sum2 = $this->request->data['summand2'];
               if (is_numeric($sum1) and is_numeric($sum2)) {
                 $result =  $this->Math->sum($sum1, $sum2);
                 $this->Flash->success("The total value is: ".$result);
               } else {
+                $result = $sum1;
                 $this->Flash->success('Some summand is not a number');
               }
           }
