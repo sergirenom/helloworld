36025925fe220cbcb90607b7fd97414a26907072
sergirenom <sergirenom@hotmail.com>
2016-10-24 12:19:58 +0200

    controller simplified

--- app/Controller/MathsController.php
+++ app/Controller/MathsController.php
@@ -3,35 +3,21 @@
   class MathsController extends AppController {
 
     public function index() {
-          if($this->request->is('post'))
-          {
-              if ($this->request->data) {
-                $sum1 = $this->request->data['summand1'];
-                $sum2 = $this->request->data['summand2'];
+      if (!empty($this->request->data)) {
+        
+        $sum1 = $this->request->data['summand1'];
+        $sum2 = $this->request->data['summand2'];
 
-                $this->sum($sum1, $sum2);
-              }
-          } elseif ($this->request->is('get')) {
-            if($this->request->query) {
-              $sum1 = $this->request->query['summand1'];
-              $sum2 = $this->request->query['summand2'];
-
-              $this->sum($sum1, $sum2);
-            }
-          }
-    //  }
-    }
-
-    function sum($sum1, $sum2) {
-      $this->set('sum1', $sum1);
-      $this->set('sum2', $sum2);
-      if (is_numeric($sum1) and is_numeric($sum2)) {
-        $result =  $this->Math->sum($sum1, $sum2);
-        $this->set('result', $result);
-      } else {
-        $this->set('error', 'Summands are not numric');
-        $this->Flash->success('Some summand is not a number');
-      }
+        $this->set('sum1', $sum1);
+        $this->set('sum2', $sum2);
+        if (is_numeric($sum1) and is_numeric($sum2)) {
+          $result =  $this->Math->sum($sum1, $sum2);
+          $this->set('result', $result);
+        } else {
+          $this->set('error', 'Summands are not numric');
+          $this->Flash->success('Some summand is not a number');
+        }
     }
   }
+}
 ?>
